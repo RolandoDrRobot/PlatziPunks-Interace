058b57010c0e04fe9db088e0fee10c05078c01a1
RolandoDrRobot <rarguello@ltvco.com>
2021-11-22 23:34:30 -0600

    Connecting to metamask

--- platzi-punks-interface/src/App.js
+++ platzi-punks-interface/src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/home/index"
+import Web3 from "web3";
 
 function App() {
+  useEffect(() => {
+    if (window.ethereum) {
+      // 👇 This is the way we request account with Metamask API
+      // window.ethereum
+      //   .request({
+      //     method: "eth_requestAccounts",
+      //   })
+      //   .then((accounts) => console.log(accounts));
+
+      // 👇 This is the way we request account with web3 JS API
+      // Web3(window.ethereum); --> window.ethereum is the provider
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  }, []);
+
   return (
     <>
       <Routes>
