85deb7776222f040914204c617a4665738b66ffe
RolandoDrRobot <rarguello@ltvco.com>
2021-11-24 00:29:52 -0600

    Consuming ABI correctly

--- platzi-punks-interface/src/App.js
+++ platzi-punks-interface/src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/home/index"
-import Web3 from "web3";
+import MainLayout from "./layouts/main";
 
 function App() {
-  useEffect(() => {
-    if (window.ethereum) {
-      // 👇 This is the way we request account with Metamask API
-      // window.ethereum
-      //   .request({
-      //     method: "eth_requestAccounts",
-      //   })
-      //   .then((accounts) => console.log(accounts));
-
-      // 👇 This is the way we request account with web3 JS API
-      // Web3(window.ethereum); --> window.ethereum is the provider
-      const web3 = new Web3(window.ethereum);
-      web3.eth.requestAccounts().then(console.log);
-    }
-  }, []);
-
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MainLayout>
     </>
   );
 }
--- platzi-punks-interface/src/views/home/index.js
+++ platzi-punks-interface/src/views/home/index.js
@@ -1,7 +1,30 @@
+import { useWeb3React } from "@web3-react/core";
+import { useCallback, useEffect, useState } from "react";
+import usePlatziPunks from "../../hooks/usePlatziPunks";
+
 const Home = () => {
+  const { active } = useWeb3React();
+  const [maxSupply, setMaxSupply] = useState();
+
+  const platziPunks = usePlatziPunks();
+
+  const getMaxSupply = useCallback(async () => {
+    if (platziPunks) {
+      const result = await platziPunks.methods.maxSupply().call();
+      setMaxSupply(result);
+    }
+  }, [platziPunks]);
+
+  useEffect(() => {
+    getMaxSupply();
+    // that second parameter for useEffect is a event listener
+  }, [getMaxSupply]);
+
+  if (!active) return "Conecta tu wallet";
+
   return (
     <>
-      <p>Hello world </p>
+      <p>Max supply: {maxSupply}</p>
     </>
   );
 };
