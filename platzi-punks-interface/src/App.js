7330a68f45ffae0f14ef352fb935b4dfcc27a4de
RolandoDrRobot <rarguello@ltvco.com>
2021-11-24 11:44:53 -0600

    Show gallery with platzi punks

--- platzi-punks-interface/src/App.js
+++ platzi-punks-interface/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./views/home/index"
+import Home from "./views/home/index";
+import Punks from "./views/punks/index"
 import MainLayout from "./layouts/main";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <MainLayout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/punks" element={<Punks />} />
         </Routes>
       </MainLayout>
     </>
